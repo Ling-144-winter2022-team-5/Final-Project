87960328a057a5d1e8a44c4fa14e44c248d61db9
kla7 <97071040+kla7@users.noreply.github.com>
2022-02-19 02:57:51 -0800

    Add files via upload

--- Data/tweepy-tweet-scraper.py
+++ Data/tweepy-tweet-scraper.py
@@ -4,12 +4,13 @@ import pandas
 # KL replace the bearer_token with your own, can get from the Twitter developer portal
 client = tweepy.Client(bearer_token='INSERT-BEARER-TOKEN')
 
-# KL this function will search for tweets including the skull emoji
+# this function will search for tweets including the skull emoji
 def skull_tweets():
   query = '\U0001F480'
 
   tweetList = []
 
+  # KL source: https://dev.to/twitterdev/a-comprehensive-guide-for-using-the-twitter-api-v2-using-tweepy-in-python-15d9 (section 4)
   for tweet in tweepy.Paginator(client.search_recent_tweets, query=query, tweet_fields=['context_annotations', 'created_at'], max_results=100).flatten(limit=1000):
     tweetList.append(tweet.text)
 
@@ -18,7 +19,7 @@ def skull_tweets():
 
   print('done with skull_tweets')
 
-# KL this function will search for tweets including the crying emoji
+# this function will search for tweets including the crying emoji
 def crying_tweets():
   query = '\U0001F62D'
 
@@ -32,6 +33,21 @@ def crying_tweets():
 
   print('done with crying_tweets')
 
-# KL runs both functions to get the csv files
+# this function will search for tweets including the moai emoji
+def moai_tweets():
+  query = '\U0001F5FF'
+
+  tweetList = []
+
+  for tweet in tweepy.Paginator(client.search_recent_tweets, query=query, tweet_fields=['context_annotations', 'created_at'], max_results=100).flatten(limit=1000):
+    tweetList.append(tweet.text)
+
+  tweetDataframe = pandas.DataFrame(tweetList)
+  tweetDataframe.to_csv('moai.csv', header=['Tweets'], index=False)
+
+  print('done with moai_tweets')
+
+# KL run above functions to get the csv files
 skull_tweets()
 crying_tweets()
+moai_tweets()
